f6eae2ba9815233bb9ddacfa7d5c6fa8e7ecdaed
M. Hadi Syatiri <syatiryhadii07@gmail.com>
2024-11-12 17:07:10 +0700

    pemrogramanII

---
+++ Pertemuan06/Praktikum/P6_1_1194049/P6_1_1194049/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace P6_1_1194049
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.vbButton = new System.Windows.Forms.Button();
+            this.csButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // vbButton
+            // 
+            this.vbButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.vbButton.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.vbButton.Location = new System.Drawing.Point(159, 133);
+            this.vbButton.Name = "vbButton";
+            this.vbButton.Size = new System.Drawing.Size(156, 45);
+            this.vbButton.TabIndex = 0;
+            this.vbButton.Text = "Visual Basic";
+            this.vbButton.UseVisualStyleBackColor = true;
+            this.vbButton.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vbButton_MouseMove);
+            // 
+            // csButton
+            // 
+            this.csButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.csButton.ForeColor = System.Drawing.Color.Red;
+            this.csButton.Location = new System.Drawing.Point(493, 133);
+            this.csButton.Name = "csButton";
+            this.csButton.Size = new System.Drawing.Size(156, 44);
+            this.csButton.TabIndex = 1;
+            this.csButton.Text = "C#";
+            this.csButton.UseVisualStyleBackColor = true;
+            this.csButton.Click += new System.EventHandler(this.csButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(49, 345);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(156, 45);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(667, 27);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Which do you think is better, Visual Basic or C#?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.csButton);
+            this.Controls.Add(this.vbButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button vbButton;
+        private System.Windows.Forms.Button csButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Pertemuan06/Praktikum/P6_3_1194049/P6_3_1194049/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace P6_3_1194049
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void SetErrorMessages(TextBox textBox, string warningMessage, string wrongMessage, string correctMessage)
+        {
+            epWarning.SetError(textBox, warningMessage);
+            epWrong.SetError(textBox, wrongMessage);
+            epCorrect.SetError(textBox, correctMessage);
+        }
+
+        private void txtHuruf_Leave(object sender, EventArgs e)
+        {
+            if (txtHuruf.Text == "")
+            {
+                SetErrorMessages(txtHuruf, "Textbox Huruf tidak boleh kosong!", "", "");
+            }
+            else if(txtHuruf.Text.All(Char.IsLetter))
+            {
+                SetErrorMessages(txtHuruf, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtHuruf, "", "Inputan hanya boleh huruf!", "");
+            }
+        }
+
+        private void txtAngka_TextChanged(object sender, EventArgs e)
+        {
+            if (txtAngka.Text == "")
+            {
+                SetErrorMessages(txtAngka, "Textbox Angka tidak boleh kosong!", "", "");
+            }
+            else if (txtAngka.Text.All(Char.IsNumber))
+            {
+                SetErrorMessages(txtAngka, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtAngka, "", "Inputan hanya boleh angka!", "");
+            }
+        }
+
+        private void txtEmail_TextChanged(object sender, EventArgs e)
+        {
+            if (txtEmail.Text == "")
+            {
+                SetErrorMessages(txtEmail, "Textbox Email tidak boleh kosong!", "", "");
+            }
+            else if (Regex.IsMatch(txtEmail.Text, @"^^[^@\s]+@[^@\s]+(\.[^@\s]+)+$"))
+            {
+                SetErrorMessages(txtEmail, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtEmail, "", "Format email salah!\nContoh: a@b.c", "");
+            }
+        }
+
+        private void txtAngka1_TextChanged(object sender, EventArgs e)
+        {
+            if (txtAngka1.Text == "")
+            {
+                SetErrorMessages(txtAngka1, "Textbox Angka tidak boleh kosong!", "", "");
+            }
+            else if (txtAngka.Text.All(Char.IsNumber))
+            {
+                SetErrorMessages(txtAngka1, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtAngka1, "", "Inputan hanya boleh angka!", "");
+            }
+        }
+
+        private void txtAngka2_TextChanged(object sender, EventArgs e)
+        {
+            if (txtAngka2.Text == "")
+            {
+                SetErrorMessages(txtAngka2, "Textbox Angka tidak boleh kosong!", "", "");
+            }
+            else if (txtAngka.Text.All(Char.IsNumber))
+            {
+                SetErrorMessages(txtAngka2, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtAngka2, "", "Inputan hanya boleh angka!", "");
+            }
+        }
+
+        private void txtAngka1_Leave(object sender, EventArgs e)
+        {
+           
+            {
+                // Validasi Angka1 wajib diisi
+                if (txtAngka1.Text == "")
+                {
+                    SetErrorMessages(txtAngka1, "Textbox Angka 1 tidak boleh kosong!", "", "");
+                    return;
+                }
+
+                // Validasi input harus angka
+                if (!txtAngka1.Text.All(Char.IsNumber))
+                {
+                    SetErrorMessages(txtAngka1, "", "Inputan hanya boleh angka!", "");
+                    return;
+                }
+
+                // Memeriksa apakah Angka2 sudah terisi
+                if (!string.IsNullOrEmpty(txtAngka2.Text))
+                {
+                    // Jika Angka2 sudah terisi, bandingkan nilainya
+                    int angka1 = Convert.ToInt32(txtAngka1.Text);
+                    int angka2 = Convert.ToInt32(txtAngka2.Text);
+
+                    if (angka1 > angka2)
+                    {
+                        SetErrorMessages(txtAngka1, "", "", "Betul!");
+                        SetErrorMessages(txtAngka2, "", "", "Betul!");
+                    }
+                    else
+                    {
+                        SetErrorMessages(txtAngka1, "", "Angka 1 harus lebih besar dari Angka 2!", "");
+                        SetErrorMessages(txtAngka2, "", "Angka 2 harus lebih kecil dari Angka 1!", "");
+                    }
+                }
+                else
+                {
+                    // Jika Angka2 belum terisi, tampilkan tanda betul untuk Angka1
+                    SetErrorMessages(txtAngka1, "", "", "Betul!");
+                }
+            }
+        }
+
+        private void txtAngka2_Leave(object sender, EventArgs e)
+        {
+            if (txtAngka2.Text == "")
+            {
+                SetErrorMessages(txtAngka2, "Textbox Angka 2 tidak boleh kosong!", "", "");
+                return;
+            }
+
+            // Validasi input harus angka
+            if (!txtAngka2.Text.All(Char.IsNumber))
+            {
+                SetErrorMessages(txtAngka2, "", "Inputan hanya boleh angka!", "");
+                return;
+            }
+
+            // Memeriksa apakah Angka1 sudah terisi
+            if (string.IsNullOrEmpty(txtAngka1.Text))
+            {
+                SetErrorMessages(txtAngka1, "Textbox Angka 1 harus diisi dahulu!", "", "");
+                SetErrorMessages(txtAngka2, "", "", "Betul!"); // Angka2 valid tapi menunggu Angka1
+                return;
+            }
+
+            // Membandingkan nilai Angka1 dan Angka2
+            int angka1 = Convert.ToInt32(txtAngka1.Text);
+            int angka2 = Convert.ToInt32(txtAngka2.Text);
+
+            if (angka1 > angka2)
+            {
+                SetErrorMessages(txtAngka1, "", "", "Betul!");
+                SetErrorMessages(txtAngka2, "", "", "Betul!");
+            }
+            else
+            {
+                SetErrorMessages(txtAngka1, "", "Angka 1 harus lebih besar dari Angka 2!", "");
+                SetErrorMessages(txtAngka2, "", "Angka 2 harus lebih kecil dari Angka 1!", "");
+            }
+        }
+    }
+}
